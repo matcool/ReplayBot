fb13a797cac1062691b0a586ee669b71108fcab7
matcool <26722564+matcool@users.noreply.github.com>
2021-02-12 14:37:39 -0300

    remove actions including x, fixes some inaccuracies

--- Replay.cpp
+++ Replay.cpp
@@ -11,7 +11,7 @@ void Replay::addAction(Action action) {
 
 void Replay::removeActionsAfterX(float x) {
 	auto check = [&](Action action) -> bool {
-		return action.x > x;
+		return action.x >= x;
 	};
 	actions.erase(std::remove_if(actions.begin(), actions.end(), check), actions.end());
 }
